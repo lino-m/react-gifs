9ae3a415c04d543c096462e8dfef7417aa8724f4
Lino Maurer <lino@q-b.de>
2020-10-26 20:19:33 +0100

    compoents: created app created search bar

--- src/index.jsx
+++ src/index.jsx
@@ -2,58 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "../assets/stylesheets/application.scss";
-
-// STEP ONE
-// // name is props -> property
-// const Hello = ({ name }) => {
-//   return (
-//     <div>
-//       Hello,
-//       {name}
-//     </div>
-//   );
-// };
-
-// STEP TWO: New formalized component
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    // Information that may change during the life-time
-    // of a component
-    // -> have to 'this' for a class component
-    this.state = {
-      clicked: false,
-    };
-  }
-
-  handleClick = () => {
-    // Change the state
-    // console.log("clicked");
-    this.setState({
-      clicked: !this.state.clicked,
-    });
-  };
-
-  render() {
-    // Build and return HTML here
-    // When you are using a cla
-    return (
-      <div
-        className={this.state.clicked ? "clicked" : null}
-        onClick={this.handleClick}
-      >
-        Hello {this.props.name}
-      </div>
-    );
-  }
-}
+import App from "./components/app.jsx";
 
 const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(
-    <Hello name="Peter" />, // Hello is the function defined above
-    root
-  ); // root is the HTML element -by ID - we bind the function to root
+  ReactDOM.render(<App />, root); // root is the HTML element -by ID - we bind the function to root
   // HTML is injected in the DIV id=root
 }
