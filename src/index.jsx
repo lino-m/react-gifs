96f309a7b25fdd7d4de8ca1cc54465a72cd3a436
Lino Maurer <lino@q-b.de>
2020-10-26 19:02:03 +0100

    Experimenting // re-familiarizing with folder structure and functionality TEST

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// STEP ONE
+// // name is props -> property
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
 
-const root = document.getElementById('root');
+// STEP TWO: New formalized component
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    // Information that may change during the life-time
+    // of a component
+    // -> have to 'this' for a class component
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  handleClick = () => {
+    // Change the state
+    // console.log("clicked");
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  };
+
+  render() {
+    // Build and return HTML here
+    // When you are using a cla
+    return (
+      <div
+        className={this.state.clicked ? "clicked" : null}
+        onClick={this.handleClick}
+      >
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <Hello name="Peter" />, // Hello is the function defined above
+    root
+  ); // root is the HTML element -by ID - we bind the function to root
+  // HTML is injected in the DIV id=root
 }
