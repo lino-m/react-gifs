10e60bfdd7f8ce0c3c3c90d93e39090bfddf83ba
Lino Maurer <lino@q-b.de>
2020-10-27 14:21:28 +0100

    dynmaic search: list of gifs on the right and side (.right-scene) is updated dynamically based on input query

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,20 +3,47 @@ import React, { Component } from "react";
 import SearchBar from "./search_bar.jsx";
 import Gif from "./gif.jsx";
 import GifList from "./gif_list.jsx";
+import giphyApi from "giphy-api";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "1Pyi8KuRGJ3AA",
+    };
+
+    this.search("coding css");
+  }
+
+  search = (query) => {
+    giphyApi("3Fgt0IweBijEW1n5MZIYLOUP3GRGTf9i").search(
+      {
+        q: query,
+        rating: "g",
+        limit: 10,
+      },
+      (error, result) => {
+        this.setState({
+          gifs: result.data,
+        });
+      }
+    );
+  };
+
   render() {
     const gifs = [{ id: "1Pyi8KuRGJ3AA" }, { id: "13FrpeVH09Zrb2" }];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar search={this.search} />
           <div className="selected-gif">
-            <Gif id="1Pyi8KuRGJ3AA" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    this.props.search(event.target.value);
+  };
+
   render() {
-    return <input type="text" className="form-control form-search"></input>;
+    return (
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
+    );
   }
 }
 
